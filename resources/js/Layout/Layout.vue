6109f574f03c1e1dc8fa3ffe21409a96d7dbb725
Ben Davis <ben@davisben.com>
2021-03-07 12:57:26 -0500

    Set up Vue and Inertia

---
+++ resources/js/Layout/Layout.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav class="navbar has-shadow" role="navigation" aria-label="main navigation">
+        <div class="container is-max-widescreen">
+            <div class="navbar-brand"><Logo /></div>
+        </div>
+    </nav>
+
+    <div class="container is-max-widescreen">
+        <slot />
+    </div>
+</template>
+
+<script>
+import Logo from '@/Components/Global/Logo'
+
+export default {
+    name: 'Layout',
+    components: {
+        Logo,
+    },
+}
+</script>
---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,15 @@
+<template>
+    <Layout></Layout>
+</template>
+
+<script>
+import Layout from '@/Layout/Layout'
+
+export default {
+    name: 'Index',
+    inheritAttrs: false,
+    components: {
+        Layout,
+    },
+}
+</script>
