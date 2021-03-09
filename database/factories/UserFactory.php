f8cdbbb77a0eb0f6d419022bb19692b893afeadb
Ben Davis <ben@davisben.com>
2021-03-09 17:06:52 -0500

    Update User factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,10 +23,11 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name,
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$argon2id$v=19$m=1024,t=2,p=2$WndkWERIMzJwczczT1hRQw$jwHjjw55Yh1Khnn8wP7+9WmsOCuJdgf2E1OLgcBeteg',
             'remember_token' => Str::random(10),
         ];
     }
